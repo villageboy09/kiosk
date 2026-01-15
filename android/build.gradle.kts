e52c586d7319d24c9bb1b5b382b752cb7c73dba3
villageboy09 <68844005+villageboy09@users.noreply.github.com>
2026-01-15 08:44:23 -0500

    Fix Java 8 obsolete warnings by forcing Java 17 for all subprojects
    Added configuration in root build.gradle.kts to:
    - Set sourceCompatibility and targetCompatibility to Java 17 for all Android subprojects
    - Set Kotlin jvmTarget to 17 for all Kotlin compilation tasks
    
    This fixes the warnings:
    - 'source value 8 is obsolete and will be removed in a future release'
    - 'target value 8 is obsolete and will be removed in a future release'

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,8 +15,29 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+// Force Java 17 for all subprojects to fix obsolete Java 8 warnings
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            extensions.configure<com.android.build.gradle.BaseExtension>("android") {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+        }
+        
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            kotlinOptions {
+                jvmTarget = "17"
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
